d016ec2204bb9564cd34c0a91a538bedd5d1682f
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-11-01 23:16:54 +0100

    added the useDispatch hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,13 +7,20 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler =()=>{
-    dispatch({type:'INCREMENT'})
+    dispatch({type:'increment'})
+  }
+  const decrementHandler =()=>{
+    dispatch({type:'decrement'})
   }
   const toggleCounterHandler = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
