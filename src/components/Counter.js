89fc4b0f3fcfa0befa372cbfa9d423f889ce77ea
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-11-02 18:16:05 +0100

    added toggle function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,20 +5,28 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const counter= useSelector(state=> state.counter)
   const dispatch = useDispatch();
+  const show = useSelector(state=> state.showCounter)
 
   const incrementHandler =()=>{
     dispatch({type:'increment'})
   }
+  const increaseHandler =()=>{
+    dispatch({type:'increaseby', amount: 5})
+  }
   const decrementHandler =()=>{
     dispatch({type:'decrement'})
   }
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
